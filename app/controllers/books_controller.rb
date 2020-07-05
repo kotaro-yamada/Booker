29e51144dbecf78b94d40d5027d1e37f727a71f8
vagrant <vagrant@localhost.localdomain>
2020-07-05 03:57:28 +0000

    first update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created"
   	  redirect_to book_path(@book.id)
     else   
-    　@books = Book.all  
+      @books = Book.all
       render :index
     end
   end
@@ -26,14 +26,14 @@ class BooksController < ApplicationController
   def destroy
   	book = Book.find(params[:id])
   	book.destroy
-  	redirect_to index_books_path
+  	redirect_to books_path
   end
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "Book was successfully updated"
-    　redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,37 @@
 Rails.application.routes.draw do
   #get '/new' => 'todolists#new'
-  get '/' => 'homes#top'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index', as:'index_books'
-  get 'books/:id' => 'books#show', as:'book'
-  get 'books/:id/edit' => 'books#edit', as:'edit_book'
-  patch 'books/:id' => 'books#update', as:'update_book'
-  delete 'books/:id' => 'books#destroy', as:'destroy_book'
- 
+  root 'homes#top'
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 
   
 
 =begin
-resources :books
-	 books_index GET    /books/index(.:format)                                                                   books#index
-               books_show GET    /books/show(.:format)                                                                    books#show
-               books_edit GET    /books/edit(.:format)                                                                    books#edit
+最新
+ Prefix Verb   URI Pattern                                                                              Controller#Action
+                     root GET    /                                                                                        homes#top
                     books GET    /books(.:format)                                                                         books#index
                           POST   /books(.:format)                                                                         books#create
-                 new_book GET    /books/new(.:format)                                                                     books#new
                 edit_book GET    /books/:id/edit(.:format)                                                                books#edit
                      book GET    /books/:id(.:format)                                                                     books#show
                           PATCH  /books/:id(.:format)                                                                     books#update
                           PUT    /books/:id(.:format)                                                                     books#update
                           DELETE /books/:id(.:format)                                                                     books#destroy
+       rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
+rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
+       rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
+update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
+     rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
+
+  
+以前  
+rescue get '/' => 'homes#top'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index', as:'index_books'
+  get 'books/:id' => 'books#show', as:'book'
+  get 'books/:id/edit' => 'books#edit', as:'edit_book'
+  patch 'books/:id' => 'books#update', as:'update_book'
+  delete 'books/:id' => 'books#destroy', as:'destroy_book' => e
+  
 =end
